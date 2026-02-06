4fcf4bd718f63f3665ea2fa169d94649c52823ee
Chanturia <chanturia007@mail.ru>
2026-02-06 22:59:54 +0300

    Добавление Pydantic-схем для токенов в schemas.py

--- backend/src/schemas.py
+++ backend/src/schemas.py
@@ -1,1 +1,11 @@
 # Pydantic-схемы
+from pydantic import BaseModel
+from typing import Optional, List
+
+# Токен
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
