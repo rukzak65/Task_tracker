c7f509975ebb5109cec81fac3ebbbf485a4718c8
Chanturia <chanturia007@mail.ru>
2026-02-08 14:58:22 +0300

    Добавление функции create_user в crud.py

--- backend/src/crud.py
+++ backend/src/crud.py
@@ -1,4 +1,15 @@
-from . import models
+from . import models, schemas, utils
+from sqlalchemy.orm import Session
 
-def get_user_by_email(db, email: str):
+
+def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = utils.get_password_hash(user.password)
+    db_user = models.User(email=user.email, password_hash=hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
