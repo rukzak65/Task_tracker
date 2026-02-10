e8a48a848fe3b60ba2ddeabd64ad204f63ce4562
Chanturia <chanturia007@mail.ru>
2026-02-10 22:13:24 +0300

    Добавление обработки ошибок для  get_current_user

--- backend/src/routes/habits.py
+++ backend/src/routes/habits.py
@@ -15,5 +15,9 @@ security = HTTPBearer()
 def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security), db: Session = Depends(get_db)):
     token = credentials.credentials
     email = verify_token(token)
+    if email is None:
+        raise HTTPException(status_code=401, detail="Invalid token")
     user = crud.get_user_by_email(db, email=email)
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
     return user
