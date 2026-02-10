a974eb452628234af989a0f4cc226c92ad9e94ea
Chanturia <chanturia007@mail.ru>
2026-02-10 22:03:50 +0300

    Добавление route для CRUD операций с привычками и функции получения текущего пользователя

--- backend/src/routes/habits.py
+++ backend/src/routes/habits.py
@@ -1,1 +1,19 @@
 # CRUD привычек
+
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from sqlalchemy.orm import Session
+import crud, schemas, models
+from database import get_db
+from utils import verify_token
+
+
+router = APIRouter()
+security = HTTPBearer()
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security), db: Session = Depends(get_db)):
+    token = credentials.credentials
+    email = verify_token(token)
+    user = crud.get_user_by_email(db, email=email)
+    return user
