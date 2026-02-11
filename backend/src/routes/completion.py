f231bbfdd4f0de030cdb38ef1dfb708863594f49
Chanturia <chanturia007@mail.ru>
2026-02-11 23:30:43 +0300

    Добавление функции для получения текущего пользователя с проверкой токена completion.py

--- backend/src/routes/completion.py
+++ backend/src/routes/completion.py
@@ -1,4 +1,22 @@
 # Отметки выполнения
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from sqlalchemy.orm import Session
+from utils import verify_token
+from .. import db_crud as crud, schemas, models
 
 router = APIRouter()
+security = HTTPBearer()
+
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security), db: Session = Depends(get_db)):
+    token = credentials.credentials
+    email = verify_token(token)
+    if email is None:
+        raise HTTPException(status_code=401, detail="Invalid token")
+    user = crud.get_user_by_email(db, email=email)
+    if user is None:
+        raise HTTPException(status_code=401, detail="User not found")
+    return user
+
+
