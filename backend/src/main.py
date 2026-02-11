886ff8250349b2ef0acef5594f7c4a3d88a3b5c7
Chanturia <chanturia007@mail.ru>
2026-02-11 22:28:52 +0300

    Добавление маршрутов для auth, habits, и заранее completion в main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from .database import init_db
+from .routes import auth, habits, completion
 
 app = FastAPI()
 
@@ -8,3 +9,8 @@ def root():
 	return {"message": "запуск"}
 
 init_db()
+
+
+app.include_router(auth.router, prefix="/auth", tags=["auth"])
+app.include_router(habits.router, prefix="/habits", tags=["habits"])
+app.include_router(completion.router, prefix="/completions", tags=["completions"])
