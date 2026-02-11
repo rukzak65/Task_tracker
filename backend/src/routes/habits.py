1630a78e39578de639ef345d5e8fef996fa734da
Chanturia <chanturia007@mail.ru>
2026-02-11 21:23:27 +0300

    добавление ручек get & post для habits

--- backend/src/routes/habits.py
+++ backend/src/routes/habits.py
@@ -21,3 +21,12 @@ def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(securit
     if user is None:
         raise HTTPException(status_code=401, detail="User not found")
     return user
+
+
+@router.get("/", response_model=list[schemas.Habit])
+def read_habits(current_user: models.User = Depends(get_current_user), db: Session = Depends(get_db)):
+    return crud.get_habits_by_user(db, current_user.id)
+
+@router.post("/", response_model=schemas.Habit)
+def create_habit(habit: schemas.HabitCreate, current_user: models.User = Depends(get_current_user), db: Session = Depends(get_db)):
+    return crud.create_habit(db, habit, current_user.id)
